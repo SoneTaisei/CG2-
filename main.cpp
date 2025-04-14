cf57acac2ec8fafb7f0511f4dfdab668f9d4c87b
SoneTaisei <k024g0009@g.neec.ac.jp>
2025-04-14 18:46:11 +0900

    crashしたときの処理を作った。

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@
 #include <Dxgi1_6.h>
 #pragma comment(lib,"dxgi.lib")
 #include <cassert>
+/* debug用の処理を作るときに使う呼び出し*/
+#include <dbghelp.h>
+#pragma comment(lib,"Dbghelp.lib")
+/* StringCcPrintfWを使うときに作る呼び出し*/
+#include<strsafe.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	/* メッセージに応じてゲーム固有の処理を行う*/
@@ -61,8 +66,33 @@ std::string str0{ "STRING" };
 
 std::string str1{ std::to_string(10) };
 
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS *exception) {
+	/* 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下に出力*/
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	wchar_t filePath[MAX_PATH] = { 0 };
+	CreateDirectory(L"./Dumps", nullptr);
+	StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d-%02d-%02d%02d.dmp", time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute);
+	HANDLE dumpFileHandle = CreateFile(filePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+	/* processId(exeID)とクラッシュ(例外)の発生したthreadIDを取得*/
+	DWORD processId = GetCurrentProcessId();
+	DWORD threadId = GetCurrentThreadId();
+	/* 設定情報を入力*/
+	MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{ 0 };
+	minidumpInformation.ThreadId = threadId;
+	minidumpInformation.ExceptionPointers = exception;
+	minidumpInformation.ClientPointers = TRUE;
+	/* Dumpを出力。MiniDumNormalは最低限の情報を出力するフラグ*/
+	MiniDumpWriteDump(GetCurrentProcess(), processId, dumpFileHandle, MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
+	/* ほかに関連付けられているSEH例外ハンドラがあれば実行。通常プロセスを終了する。*/
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
 /* windowsアプリでのエントリーポイント(main関数)*/
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	/* 予期せぬ事態(Unhandled)に補足する関数を登録*/
+	SetUnhandledExceptionFilter(ExportDump);
 
 	WNDCLASS wc{};
 	/* ウィンドウプロシージャ*/
@@ -172,6 +202,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	/* デバイスの生成がうまくいかなかったので起動できない*/
 	assert(device != nullptr);
 	Log("Complete create D3D12Devie!!!\n");
+
+	
 
 	MSG msg{};
 	/* ウィンドウのxボタンが押されるまでループ*/

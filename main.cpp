4c9d535259c871362b740c76e387fbd3547029be
SoneTaisei <k024g0009@g.neec.ac.jp>
2025-04-23 17:04:35 +0900

    三角形を動かすために途中までやった。02_02_8Pから

--- main.cpp
+++ main.cpp
@@ -537,10 +537,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
   
   // RootParameter作成。複数設定できるので。今回は結果1つだけなので長さ1の配列
-  D3D12_ROOT_PARAMETER rootParameters[1] = {};
+  D3D12_ROOT_PARAMETER rootParameters[2] = {};
   rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
   rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
   rootParameters[0].Descriptor.ShaderRegister = 0;// レジスタ番号0とバインド
+  rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
+  rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;// VertexShaderで使う
+  rootParameters[1].Descriptor.ShaderRegister = 0;// レジスタ番号0を使う
   descriptionRootSignature.pParameters = rootParameters;// ルートパラメータ配列へのポインタ
   descriptionRootSignature.NumParameters = _countof(rootParameters);// 配列の長さ
   

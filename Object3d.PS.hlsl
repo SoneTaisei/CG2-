9e1f317122405d4ec6adc711a6daf53567109c72
SoneTaisei <k024g0009@g.neec.ac.jp>
2025-06-16 14:25:41 +0900

    HalfLambertの式を使いライティングの影を滑らかにした。

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -43,7 +43,9 @@ float4 main(VertexShaderOutput input) : SV_TARGET {
     float4 color;
 
     if (gMaterial.enableLighting != 0) {
-        float cos = saturate(dot(normal, lightDir));
+        // HalfLambertでライティングの影を滑らかにする
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     } else {
         color = gMaterial.color * textureColor;
